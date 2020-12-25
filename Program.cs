eb633e314737f9911b87e96b48a1e1ba5e9191d7
Yoichi Izumi <quanyangyi0323@gmail.com>
2020-12-25 22:54:47 +0000

    [mdf] XMLコメント用に設定変更

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ namespace csharp_template
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
